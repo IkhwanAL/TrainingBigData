719aa9c7ffe030b0ca6ed3adac2cc2842122faa1
Ikhwan A Latif <ikhwanal235@gmail.com>
2021-03-22 22:18:05 +0700

    Change 2

--- Route/Employee/employee.js
+++ Route/Employee/employee.js
@@ -1,8 +1,23 @@
 const employee = require('express').Router;
+const {Con} = require('../../connection');
 require('dotenv/config');
 
-employee.get('/', (req, res) => {
-    
+const memo = {};
+
+var findUser = (tableName, option = {field: [], filterOption = []}) => {
+    let {field, filterOption} = option;
+    return new Promise((resolve, reject) => {
+        let findUser = '';
+        if(field.length == 0){
+            findUser += `SELECT * from ${tableName}`;
+        }else{
+            
+        }
+    })
+}
+
+employee.get('/', async (req, res) => {
+
 })
 
 module.exports = {employee}
