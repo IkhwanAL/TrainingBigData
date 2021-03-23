468846169425b8a6306e6ce5cc02f52d0ac5d8ce
Kuro_X12 <ikhwanal235@gmail.com>
2021-03-23 12:58:49 +0700

    change through laptop

--- Route/Employee/employee.js
+++ Route/Employee/employee.js
@@ -1,10 +1,16 @@
-const employee = require('express').Router;
+const express = require('express');
+const employee = express.Router();
 const {Con} = require('../../connection');
+const cors = require('cors');
+const redis = require('redis');
 require('dotenv/config');
 
-const memo = {};
+const client = redis.createClient();
+client.on('error', (er) => {
+    console.log(er)
+})
 
-var findUser = (tableName, option = {field: [], filterOption = []}) => {
+const findUser = (tableName, option = {field: [], filterOption: []}) => {
     let {field, filterOption} = option;
     return new Promise((resolve, reject) => {
         let findUser = '';
@@ -16,8 +22,43 @@ var findUser = (tableName, option = {field: [], filterOption = []}) => {
     })
 }
 
-employee.get('/', async (req, res) => {
+const getData = async (key, callback) => {
+    let connection = await Con();
+    connection.query('SELECT * FROM salaries', (er, rows, fields) => {
+        if(er) throw er;
 
+        client.setex(key, 3600, JSON.stringify(rows))
+
+        return callback(rows);
+    })
+}
+
+// employee.use(cors());
+
+employee.get('/', async (req, res) => {
+    console.log('im in')
+    const setKeyEmployee = 'emp';
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Request-Method', 'GET');
+    let data = await Con();
+    // data.query('SELECT * FROM salaries', (er, row, fil) => {
+    //     if(er) throw er;
+    //     res.send(row)
+    // })
+    // res.json(result)
+    return client.get(setKeyEmployee, (err, data) => {
+        if(err) throw err;
+
+        if(data){
+            return res.json({data: JSON.parse(data)})
+        }else{
+            let result;
+            getData(setKeyEmployee, (stuff) => {
+                result = stuff;
+            })
+            return res.json({data: result});
+        }
+    })
 })
 
 module.exports = {employee}
--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const exp = require('express');
+const {Con} = require('./connection');
 const app = exp();
-const Employee = require('./Route/Employee/employee');
+const event = require('events').EventEmitter;
+const {employee} = require('./Route/Employee/employee');
 
 require('dotenv/config');
 
 const api = process.env.API_URL;
 
 app.use(exp.urlencoded({extended: true}));
-app.use(`${api}/Employee`, Employee);
+app.use('/emp', employee);
 
-app.listen(process.env.EXPRESS_PORT);
+const getSalaries = async() => {
+    let connection = await Con();
+    // connection.query('select * FROM salaries', (er, row, field) => {
+    //     console.log(row);
+    // })
+}
+
+app.listen(process.env.EXPRESS_PORT, () => {
+    getSalaries();
+});
